29b17d51a74a136c8f5bab74d90c7539e89033b6
tsuruo <tsuru.dev@gmail.com>
2022-09-03 17:29:50 +0900

    FirebaseAuthExceptionでblocking functionsをcatchできることを確認

--- flutter/lib/login_page.dart
+++ flutter/lib/login_page.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_force_email_verification/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:flutterfire_ui/auth.dart';
 
 class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
@@ -8,13 +10,30 @@ class LoginPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign In')),
+      appBar: AppBar(title: const Text('Sign Up')),
       body: Center(
-        child: SizedBox(
-          height: 44,
-          child: SignInButton(
-            Buttons.GoogleDark,
-            onPressed: () {},
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 44),
+          child: EmailForm(
+            action: AuthAction.signUp,
+            onSubmit: (email, password) async {
+              try {
+                final credential =
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                logger.info(credential);
+                // Blocking Functionsで弾かれた場合はFirebaseAuthExceptionでcatchできるものの
+                // `code`はundefinedになってしまっていて、全てmessageにStringで入ってしまうので注意。
+                // 改善されるとは思いつつ。
+              } on FirebaseAuthException catch (e) {
+                final isBlocking = e.message?.contains(
+                  'BLOCKING_FUNCTION_ERROR_RESPONSE',
+                );
+                logger.info('isBlocking: $isBlocking');
+              }
+            },
           ),
         ),
       ),
