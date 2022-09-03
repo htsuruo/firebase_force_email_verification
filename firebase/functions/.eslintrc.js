0f3e872093cdd177b1c4f50613571150f0a76648
tsuruo <tsuru.dev@gmail.com>
2022-09-03 15:40:41 +0900

    Setup prettier

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -5,27 +5,30 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    // 'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*', // Ignore built files.
   ],
   plugins: [
-    "@typescript-eslint",
-    "import",
+    '@typescript-eslint',
+    'import',
+    'prettier',
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
+    'prettier/prettier': ['error', {singleQuote: true, semi: false, tabWidth: 2}],
+    'camelcase': ['warn', {properties: 'never'}],
+    'import/no-unresolved': 0,
   },
 };
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,13 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const beforeCreate = functions.auth
+  .user()
+  .beforeCreate((user, context) => {
+    // TODO(tsuruoka):
+  })
+
+export const beforeSignIn = functions.auth
+  .user()
+  .beforeSignIn((user, context) => {
+    // TODO(tsuruoka):
+  })
