dbe4cf2db3ef6b9a325c47312bc78fb5f93e142b
tsuruo <tsuru.dev@gmail.com>
2022-09-03 21:42:57 +0900

    MailSenderの作成や秘密情報をSecretManagerに

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,26 +1,26 @@
 import * as functions from 'firebase-functions'
-// import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin'
 
-// import { createTransport, SendMailOptions, SentMessageInfo } from 'nodemailer'
+import { createTransport, SendMailOptions, SentMessageInfo } from 'nodemailer'
+import { AuthUserRecord } from 'firebase-functions/lib/common/providers/identity'
 
+admin.initializeApp()
 // const bundleId = 'com.tsuruoka.firebaseForceEmailVerification'
 // const packageName = 'com.tsuruoka.firebase_force_email_verification'
 
-export const beforeCreate = functions.auth
-  .user()
-  .beforeCreate(async (user, _) => {
-    functions.logger.info('beforeCreate trigger!')
-    if (user.email && !user.emailVerified) {
-      // 本来は確認用のメールを送信するべきだがSMTPサーバを用意したりメール送信の処理は本質ではないのでなので省略
-      throw new functions.auth.HttpsError(
-        'invalid-argument',
-        `"${user.email}" required verification.`
-      )
+export const beforeCreate = functions
+  .runWith({ secrets: ['GMAIL_USER', 'GMAIL_PASS'] })
+  .auth.user()
+  .beforeCreate(async (user, context) => {
+    const locale = context.locale
+    const email = user.email
+    if (email && !user.emailVerified) {
+      const link = await admin.auth().generateEmailVerificationLink(email)
+      sendCustomVerificationEmail({ user, link, locale })
     }
   })
 
 export const beforeSignIn = functions.auth.user().beforeSignIn((user, _) => {
-  functions.logger.info('beforeSignIn trigger!')
   if (user.email && !user.emailVerified) {
     throw new functions.auth.HttpsError(
       'invalid-argument',
@@ -29,28 +29,74 @@ export const beforeSignIn = functions.auth.user().beforeSignIn((user, _) => {
   }
 })
 
-// export const sendCustomVerificationEmail = functions.https.onRequest(
-//   async (_, __): Promise<SentMessageInfo> => {
-//     const transporter = createTransport({
-//       host: 'smtp.gmail.com',
-//       port: 465,
-//       secure: true, // SSL
-//       auth: {
-//         user: 'hideki.tsuruoka.fb@gmail.com',
-//         pass: 'hideki226fb',
-//       },
-//     })
-//     const options: SendMailOptions = {
-//       from: 'hideki.tsuruoka.fb@gmail.com',
-//       to: 'haribari920@gmail.com',
-//       subject: '{件名}',
-//       text: '{本文}',
-//     }
-//     const response = await transporter.sendMail(options)
-//     functions.logger.info(`${response.messageId}: ${response.response}`)
-//     if (response.rejected) {
-//       functions.logger.warn('sendCustomVerificationEmail error.')
-//     }
-//     return response
-//   }
-// )
+export const sendTestMail = functions
+  .runWith({ secrets: ['GMAIL_USER', 'GMAIL_PASS'] })
+  .https.onRequest(async (req, resp): Promise<SentMessageInfo> => {
+    functions.logger.warn(process.env.GMAIL_USER)
+    await MailSender.instance.send({
+      to: req.body.email,
+      subject: 'Test Mail',
+      text: `This is a test message.
+
+これは。
+
+これはテストメッセージです。
+`,
+    })
+    return resp.sendStatus(200)
+  })
+
+async function sendCustomVerificationEmail(param: {
+  user: AuthUserRecord
+  link: string
+  locale: string | undefined
+}): Promise<SentMessageInfo> {
+  // TODO(tsuruoka): 本来はlocaleに応じて表示言語を切り替えるだが省略
+  const mail: Mail = {
+    to: param.user.email!,
+    subject: 'メールアドレスの確認',
+    text: `${param.user.displayName} 様
+
+メールアドレスを確認するには、次のリンクをクリックしてください。
+
+${param.link}
+
+このアドレスの確認を依頼していない場合は、このメールを無視してください。
+
+よろしくお願いいたします。
+`,
+  }
+  return await MailSender.instance.send(mail)
+}
+
+class MailSender {
+  static get instance() {
+    return (this._instance ??= new MailSender())
+  }
+  private static _instance: MailSender | undefined
+
+  async send(mail: Mail): Promise<SentMessageInfo> {
+    const transporter = createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+      },
+    })
+    const options: SendMailOptions = {
+      from: 'noreply@gmail.com',
+      to: mail.to,
+      subject: mail.subject,
+      text: mail.text,
+    }
+    const response = await transporter.sendMail(options)
+    functions.logger.info(`${response.messageId}: ${response.response}`)
+    return response
+  }
+}
+
+interface Mail {
+  readonly to: string
+  readonly subject: string
+  readonly text: string
+}
